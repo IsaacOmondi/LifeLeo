839bf69cf5b9901af0b97776ade2d281d50052ee
Isaac Omondi <iomondi90@gmail.com>
2025-01-08 16:48:55 +0300

    feat: Add google sign in option

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,32 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/google/redirect', async ({ ally }) => {
+    return ally.use('google').redirect()
+})
+
+router.get('/google/callback', async ({ ally }) => {
+    const google = ally.use('google')
+
+    if (google.accessDenied()) {
+        return 'You have cancelled the login process'
+    }
+
+    /**
+     * OAuth state verification failed. This happens when the CSRF cookie gets expired
+     */
+    if (google.stateMisMatch()) {
+        return 'We are unable to verify the request. Please try again'
+    }
+    
+    /**
+     * GitHub responded with some error
+     */
+    if (google.hasError()) {
+        return google.getError()
+    }
+  
+    const user = google.user()
+    return user
+  })
