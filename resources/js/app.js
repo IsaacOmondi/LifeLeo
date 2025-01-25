7e69a5341aa5cc6e230dce6cf83e9ed51b97046d
Isaac Omondi <iomondi90@gmail.com>
2025-01-25 15:08:19 +0300

    feat: bootstrap setup and wip landing page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+function toggleBrightnessMode() {
+  const html = document.getElementById('htmlPage')
+  const brightnessMode = document.getElementById('toggleBrightnessMode');
+  const setMode = brightnessMode.getAttribute('set-mode')
+  if (setMode == 'light') {
+    html.setAttribute('data-bs-theme', 'dark')
+  } else if (setMode == 'dark') {
+    html.setAttribute('data-bs-theme', 'light')
+  }
+}
+
+
+
+const brightnessMode = document.getElementById('toggleBrightnessMode');
+brightnessMode.addEventListener('click', toggleBrightnessMode)
